976a2846ede62a9efb561c19d3b747da2cb44419
Ewan <ebreakey@gmail.com>
2025-12-01 17:07:41 +1100

    Add lua test runner

---
+++ day1/test.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# Run the program
+OUTPUT=$(luajit ./main.lua sample.txt)
+
+if [ $? -ne 0 ]; then
+	echo "OUTPUT:"
+	echo "$OUTPUT"
+	exit 1
+fi
+
+# Read the expected output file
+EXPECTED=$(cat sample-out.txt)
+
+# Compare
+clear
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+	echo "✅"
+else
+	echo "Mismatch!"
+	echo "---- OUTPUT ----"
+	echo "$OUTPUT"
+	echo "---- EXPECTED ----"
+	echo "$EXPECTED"
+fi
